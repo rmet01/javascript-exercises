ba33e77512f1ca78d93855d1a37147b5e1843323
rmet01 <293993125+rmet01@users.noreply.github.com>
2026-07-08 21:41:11 +0300

    Complete foundations excercises 15-16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = arr.sort((a , b) => (b.yearOfDeath - b.yearOfBirth || 2026 - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth || 2026 - b.yearOfBirth))
+    return oldest[0]
 };
 
 // Do not edit below this line
