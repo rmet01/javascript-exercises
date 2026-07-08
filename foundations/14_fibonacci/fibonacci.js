7b51de1e1308dea172afe479ca52d3fbcbc89fac
rmet01 <293993125+rmet01@users.noreply.github.com>
2026-07-08 20:47:25 +0300

    Complete 14_fibonacci excercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    const arr = [0,1];  
+
+    if (Number(a)>=2) {
+        for (let count = 2; count <= Number(a); count++){
+            arr.push(arr[(count-1)]+arr[count-2])
+        };
+    }
+    else if (Number(a)<0) return "OOPS"
+    return arr[Number(a)]          
 
 };
 
