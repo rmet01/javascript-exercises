68917ae397201367db7b39a7ef5fb482a6d32602
rmet01 <293993125+rmet01@users.noreply.github.com>
2026-07-08 17:10:58 +0300

    Complete 13_palindromes excercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let noSpaces = word.replaceAll(/[ ,.!]/g, "");
+    let revWord = noSpaces.split("").reverse().join("");
+    if (noSpaces.toLowerCase() === revWord.toLowerCase())
+        return true
+    else return false
 };
 
 // Do not edit below this line
